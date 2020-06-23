fc28be3a1d9a49f83044bf7ee0c35f245eeebcbc
Ndriçim Hajrullahu <ndriqimh1999@gmail.com>
2020-06-23 14:00:23 +0200

    Krijmi i nje Welcome/Splash Screen me nje logo kur hapet aplikacioni

---
+++ app/src/main/java/com/fiek/androidapp/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.fiek.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
+
